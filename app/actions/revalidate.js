b47378533d39e40fa46d42b04ea5235b2b776525
Whiz7z <gjevstratov@gmail.com>
2024-06-28 20:56:42 +0100

    debug 5

--- app/actions/revalidate.js
+++ app/actions/revalidate.js
@@ -28,3 +28,27 @@ export async function editProfileAction(data) {
   revalidateTag("profile");
   redirect("/profile");
 }
+
+
+// export async function editProfileAction(data) {
+//   const filePath = path.join(process.cwd(), "profileData", "profileData.json");
+
+//   // Ensure the directory exists
+//   if (!fs.existsSync(path.dirname(filePath))) {
+//     fs.mkdirSync(path.dirname(filePath), { recursive: true });
+//   }
+
+//   try {
+//     await fs.promises.writeFile(filePath, JSON.stringify(data, null, 2));
+//     console.log("File written successfully");
+//   } catch (err) {
+//     console.error("Error writing to file", err);
+//     return {
+//       success: false,
+//       message: "Error writing to file",
+//     };
+//   }
+
+//   revalidateTag("profile");
+//   redirect("/profile");
+// }
--- app/api/get-profile-data/route.js
+++ app/api/get-profile-data/route.js
@@ -2,11 +2,38 @@ import { NextResponse } from "next/server";
 import fs from "fs";
 import path from "path";
 
+// export async function GET() {
+//   let returnPath;
+//   try {
+//     console.log("process.cwd -- ", process.cwd());
+//     const filePath = path.join(process.cwd(), "/profileData/profileData.json");
+//     returnPath = filePath;
+
+//     console.log("filepath -- ", filePath);
+
+//     const data = await fs.promises.readFile(filePath, "utf-8");
+
+//     console.log("data in api -- ", JSON.parse(data));
+//     return NextResponse.json(JSON.parse(data));
+//   } catch (error) {
+//     console.error("Error reading file:", error);
+//     return NextResponse.json(
+//       {
+//         success: false,
+//         message: "Failed to read profile data.",
+//         path: returnPath,
+//       },
+//       { status: 500 }
+//     );
+//   }
+// }
+
+
 export async function GET() {
   let returnPath;
   try {
     console.log("process.cwd -- ", process.cwd());
-    const filePath = path.join(process.cwd(), "/profileData/profileData.json");
+    const filePath = path.join("/tmp", "profileData", "profileData.json");
     returnPath = filePath;
 
     console.log("filepath -- ", filePath);
