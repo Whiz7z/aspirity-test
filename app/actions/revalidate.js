e88285827d9475c809d558ee8abd9df918c71105
Whiz7z <gjevstratov@gmail.com>
2024-06-28 20:49:36 +0100

    debug 5

--- app/actions/revalidate.js
+++ app/actions/revalidate.js
@@ -7,15 +7,24 @@ import fs from "fs";
 import path from "path";
 
 export async function editProfileAction(data) {
+  const filePath = path.join(process.cwd(), "profileData", "profileData.json");
 
-  const filePath = path.join(process.cwd(), "/profileData/profileData.json");
+  // Ensure the directory exists
+  if (!fs.existsSync(path.dirname(filePath))) {
+    fs.mkdirSync(path.dirname(filePath), { recursive: true });
+  }
 
-  fs.writeFile(filePath, JSON.stringify(data, null, 2), (err) => {
-    if (err) {
-      console.error("Error writing to file", err);
-    }
-  });
+  try {
+    await fs.promises.writeFile(filePath, JSON.stringify(data, null, 2));
+    console.log("File written successfully");
+  } catch (err) {
+    console.error("Error writing to file", err);
+    return {
+      success: false,
+      message: "Error writing to file",
+    };
+  }
 
-  revalidateTag("profile", "page");
+  revalidateTag("profile");
   redirect("/profile");
 }
